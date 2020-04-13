5bb2024caee1ad9d22879e76663db39be9dbfaa0
Obie Fernandez <obiefernandez@gmail.com>
2020-04-13 16:40:06 -0500

    styles now somewhat working (wip)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,12 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+
+// stylesheets
+import 'stylesheets/mercury';
+import 'stylesheets/normalize';
+import 'stylesheets/cask';
+import 'stylesheets/reset';
+import 'stylesheets/main'
